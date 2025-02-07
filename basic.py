7331d81a81cbbc129920e95fa5d1fc17aee277de
Luke Wallace <lwallace756@students.ccsnh.edu>
2025-02-06 20:38:13 -0500

    token and lexer

---
+++ basic.py
@@ -0,0 +1,44 @@
+#Token
+
+TT_INT      = 'TT_INT'
+TT_FLOAT    = 'FLOAT'
+TT_PLUS     = 'PLUS'
+TT_MINUS    = 'MINUS'
+TT_MUL      = 'MUL'
+TT_DIV      = 'DIV'
+TT_LPAREN   = 'LPAREN'
+TT_RPAREN   = 'RPAREN'
+
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.type = value
+    
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+    
+
+#LEXER
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[pos] if self.pos < len(self.text) else None
+    
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in '\t':
+                self.advance()
