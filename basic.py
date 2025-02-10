58c8824c1ad9327c94231f9f4f271885ea06de0c
Luke Wallace <lwallace756@students.ccsnh.edu>
2025-02-10 14:01:58 -0500

    string errors

--- basic.py
+++ basic.py
@@ -24,6 +24,9 @@ class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
+class InvalidSyntaxError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Invalid Syntx', details)
 #######################################
 # POSITION
 #######################################
