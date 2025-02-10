4fb717b582e72e8d16ff6bf375bb98bce0dc2cc4
Luke Wallace <lwallace756@students.ccsnh.edu>
2025-02-10 14:28:30 -0500

    parse result

--- basic.py
+++ basic.py
@@ -201,6 +201,32 @@ class BinOpNode:
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node} )'
     
+#######################################
+#Parse Result 
+######################################
+class ParseResult:
+    def __init__ (self):
+        self.error = None
+        self.node = None
+
+    def register(self, res):
+        if isinstance(res, ParseResult):
+            if res.error: self.error = res.error
+            return res.node
+        
+        return res
+    def success(self, node):
+        self.node = node
+        return self
+        
+
+    def failure(self, error):
+        self.error = error
+        return self
+
+
+
+
 
 #######################################
 #PARSER
