adaaddada3ec994160d6996aff0f08e6e035b570
Luke Wallace <lwallace756@students.ccsnh.edu>
2025-02-10 13:48:39 -0500

    parser

--- basic.py
+++ basic.py
@@ -190,10 +190,43 @@ class BinOpNode:
 #######################################
 
 class Parser:
-    def __init__(tokens):
-        self.tokens = tokens 
-        self.tok_idx = 1
-        self.advance()
+    def __init__(self,tokens):
+      self.tokens = tokens 
+      self.tok_idx = 1
+      self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+    
+
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+    
+    def expr(self):
+        return self.bin_op(self.factor, (TT_PLUS, TT_MINUS))
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok in ops:
+            op_tok = self.current_tok
+            right = func()
+            left = BinOpNode (left, op_tok, right)
+        return left
+
+        
+        
         
         
 
